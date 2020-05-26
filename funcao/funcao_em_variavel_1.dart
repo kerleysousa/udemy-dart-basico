46d05c6cef01d80ad5a44df2d7186264e81cf337
Kerley <kerley_sousa@live.com>
2020-05-25 21:19:27 -0300

    Correção do retorno da função.

--- funcao/funcao_em_variavel_1.dart
+++ funcao/funcao_em_variavel_1.dart
@@ -3,10 +3,11 @@ main (){
   somaFn(10, 20);
 
   int Function (int x, int y) soma1 = somaFn;
-  soma1(30, 30);
+  int resultado = soma1(30, 30);
+  print(resultado.toString());
 
   }
 
 int somaFn (int x, int y) {
-  print("${x.toString()} + ${y.toString()} = ${x+y}");
+  return x+y;
 }
